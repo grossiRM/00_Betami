07a292a4fc5aff49ec55aaeeee188e0fcefef955
Rodrigo <grossiRM@hotmail.com>
2024-12-31 13:43:23 -0300

    a1

---
+++ 01_GMDSI_____advanced/01_Tutorials___AGAIN___/part2_02_OW__Reimagined__y/C_____Beta_weights_vs_noise__see__.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exploration of adding noise to observations (or not) when using the Iterative Ensemble Smoother (iES)
+
+# ### We can start out with an objective function.
+
+# Let's make a couple definitions:
+# $\boldsymbol{y}$ are the observation values
+# $\boldsymbol{X}$ is the action of the model and $\boldsymbol{\beta}$ is the current set of model parameters  
+# Then, $\mathbf{y}\mathbf{-}\mathbf{X}\boldsymbol{\beta}$ is the discrepancy (e.g. residual or error) between model outputs and observations.
+# 
+# We want to minimize those, and for a variety of reasons, we actually want to minimize the square of those errors, and that set of squared errors is $\Phi$, called the "objective function"
+# 
+# $$\Phi\left(\boldsymbol{\beta}\right) =  \left(\mathbf{y}\mathbf{-}\mathbf{X}\boldsymbol{\beta}\right)^{T}\left(\mathbf{y}\mathbf{\mathbf{-}}\mathbf{X}\boldsymbol{\beta}\right) = \sum_{i=1}^{N_{obs}}\left( y_i - \sum_{j=1}^{N_{par}} X_{ij}\beta_{j}\right)^2$$
+# 
+# But....we know that not each observation is of equal value/information/importance/certainty, so we can use weights to amplify or suppress the contributions from specific observations. We can assign weights as:  
+# $$\omega=\frac{1}{\sigma}$$
+# or, in matrix form, the weights are defined as $\boldsymbol{Q}$
+# $$\Phi\left(\boldsymbol{\beta}\right)=\left(\mathbf{y}\mathbf{-}\mathbf{X}\boldsymbol{\beta}\right)^{T}\mathbf{Q}\left(\mathbf{y}-\mathbf{X}\boldsymbol{\beta}\right) = \sum_{i=1}^{N_{obs}}\left(\omega_i \left(y_i - \sum_{j=1}^{N_{par}} X_{ij}\beta_{j}\right)\right)^2$$  
+# 
+
+# In[1]:
+
+
+from wvn_helper import plot_mod_obs
+
+
+# Without observation noise, we hope that the modeled output distribution will overlap (or "cover") the single observation value.
+
+# In[2]:
+
+
+plot_mod_obs(6)
+
+
+# If the modeled output distribution does _not_ overlap the observed value, that observation is in "prior data conflict".
+
+# In[3]:
+
+
+plot_mod_obs(11,pdc=True)
+
+
+# But what if we set an observation weight for the obs? 
+# 
+# Based on general background, we assume 
+# $\omega \approx \frac{1}{\sigma}$
+# 
+# So, sampling the noise around the observation results in 
+# $\sigma = \frac{1}{\omega}$
+# 
+# For example, if this is a head observation, 
+# $\sigma=0.5m$ is a reasonable starting point might be $\omega=\frac{1}{.5}=2.0$
+# 
+
+# In[4]:
+
+
+plot_mod_obs(6, noisy=True, std=.5)
+
+
+# In[5]:
+
+
+plot_mod_obs(11, noisy=True, std=.5, pdc=True)
+
+
+# That's all good, but what if we adjust weights to balance the objective function? This is a common approach and perfectly defensible, but say we decrease the weight on the heads quite a bit. is $\omega \approx \frac{1}{\sigma}$ still valid?
+# 
+# If we adjust $\omega$ to be $0.2$ then $\sigma = \frac{1}{\omega} = \frac{1}{0.2} = 5.0$
+
+# In[6]:
+
+
+plot_mod_obs(11, noisy=True, std=[.5,5] )
+
+
+# So, there are pretty important implications in how we choose observation noise. This can make the difference between a realistic representation of the noise and an unrealistic (and really, uninformative) level of spread in the observation values. Further, this decision can make the difference between concluding that an observation is in prior-data-conflict or not. With many observations, the implications of this are very important.
---
+++ 04_PEST_pp________On_the_run-01/benchmarks/01_one_dim_flow/c_Beta_Script_02.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os, sys
+import numpy as np
+import pandas as pd
+import flopy
+
+
+# In[2]:
+
+
+def forward_model():
+    try:
+        os.remove("Beta_02/heads_out.csv")
+    except:
+        pass
+    
+    indx = pd.read_csv('Beta_02/index.csv')['idx'].values.astype(int)   
+    
+    
+    x = np.loadtxt('Beta_02/inputx.dat')
+    x = np.power(10.0, x)
+    mf = flopy.modflow.Modflow.load(r'flow_1d.nam', model_ws = 'Beta_02/Beta_03' )
+    
+    hk = mf.upw.hk.array.copy()
+    hk = x[np.newaxis, np.newaxis, :]
+    mf.upw.hk = hk
+    mf.upw.write_file()
+    
+    basefolder = os.getcwd()
+    os.chdir("Beta_02")
+    os.system("mfnwt.exe flow_1d.nam")
+    os.chdir(basefolder)
+
+    hds = flopy.utils.HeadFile(os.path.join('Beta_02/Beta_03', 'flow_1d.hds'))
+    wl = hds.get_data(totim=1.0)
+    wl = wl.squeeze()
+    y =wl[indx]                                                 # model maping  
+
+    out = pd.DataFrame()
+    out['y'] = y
+    out.to_csv('heads_out.csv', index_label = 'id')             # write model output
+    
+if __name__ == "__main__":
+    forward_model()
+
+
+# #### if working 
+
+# In[ ]:
+
+
+get_ipython().system('jupyter nbconvert --to script c_Beta_Script_02.ipynb')
+
+
+# In[ ]:
+
+
+
+
