c91d5e1a16bac9803d82eca6fbe0d4758fe962d7
Rodrigo <grossiRM@hotmail.com>
2025-07-01 16:18:55 -0300

    130

---
+++ 00_MF6/_next/example.py
@@ -0,0 +1,26 @@
+def function_one (a1, a2, a3): 
+    a = bb
+    return c
+def function_two(): 
+    dd = function_one
+def function_three(f1, f2): 
+    ggg = function_two
+    return h
+
+
+
+
+
+c=c
+h = 
+
+
+
+
+ggggg = '11'
+bb = '11'
+ccc = '11' 
+a1 = '11'
+
+
+and
